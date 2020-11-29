7508834229114d27aaa1e545371deb3c7cd4d566
Ramon Pacheco <ramon_pacheco_42@hotmail.com>
2020-11-29 18:22:43 -0300

    Version 0.1

---
+++ pessoas/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Pessoa
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ('id','nome', 'email')
+    list_display_links = ('nome',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+admin.site.register(Pessoa, ListandoPessoas)
+
